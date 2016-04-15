06be3f9864eb6a1163d8cd5700b8e9efc05aecde
srhm501 <srhm501@york.ac.uk>
2016-04-15 16:27:02 +0100

    Update energy.sh
    Added comment saying how to use

--- energy.sh
+++ energy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# finds energy from file
+# call as "./energy.sh file"
+
+
 FILENAME=$1
 STRING=$(grep "Total energy corrected for finite basis set" $FILENAME)
 ENERGY=$(echo $STRING | awk '{print $9}')
