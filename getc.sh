897b9a5724fab8d8dc795f9186bc00dee9aad986
srhm501 <srhm501@york.ac.uk>
2016-04-19 13:03:22 +0100

    Update getc.sh
    ReFixed

--- getc.sh
+++ getc.sh
@@ -5,8 +5,8 @@ for file in *.cell
 do
 name=${file::-5}
 grep "^     O" $name | awk '{$1="O";sub("O","");print}' > O_$name.dat
-grep "^    Mg" $name | awk '{$1="O";sub("O","");print}' > Mg_$name.dat
-grep "^    Ca" $name | awk '{$1="O";sub("O","");print}' > Ca_$name.dat
+grep "^    Mg" $name | awk '{$1="Mg";sub("Mg","");print}' > Mg_$name.dat
+grep "^    Ca" $name | awk '{$1="Ca";sub("Ca","");print}' > Ca_$name.dat
 done
 
 for FILE in *.dat; do
