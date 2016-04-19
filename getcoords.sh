acf1a2adea7262d5786e8db46763f20c64107db5
srhm501 <srhm501@york.ac.uk>
2016-04-19 12:55:40 +0100

    Fixed

--- getcoords.sh
+++ getcoords.sh
@@ -2,5 +2,5 @@
 
 # call as "./getcoords.sh file.cell"
 
-grep "^     O" $1 > O.dat
-grep "^    Mg" $1 > Mg.dat
+grep "^     O" $1 | awk '{$1="O"; sub("O",""); print}' > O.dat
+grep "^    Mg" $1 | awk '{$1="Mg"; sub("Mg",""); print}' > Mg.dat
