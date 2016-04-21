091ea0129b946e0a99bb5dea2a85c549fe4cd3e7
SRH Morris <srhm501@tlabs180.its.york.ac.uk>
2016-04-21 13:13:40 +0100

    added fortran code to output random atoms plus makefile.

--- src/rand.f90
+++ src/rand.f90
@@ -5,27 +5,19 @@ integer, parameter :: dp = selected_real_kind(15,300)
 
 contains
 
-subroutine init_random_seed()
-  implicit none
-  integer, allocatable :: seed(:)
-  integer :: n, un
-  call random_seed(size=n)
-  allocate(seed(n))
+  subroutine init_random_seed()
+    implicit none
+    integer, allocatable :: seed(:)
+    integer :: n, un
+    call random_seed(size=n)
+    allocate(seed(n))
 
-  open(newunit=un, file="/dev/urandom", access="stream", &
-       & form="unformatted", action="read", status="old")
-  read(un) seed
-  close(un)
-  
-  call random_seed(put=seed)
-end subroutine init_random_seed
+    open(newunit=un, file="/dev/urandom", access="stream", &
+         & form="unformatted", action="read", status="old")
+    read(un) seed
+    close(un)
 
-function get_rand_coord(max_val) result(coord)
-  implicit none
-  integer :: max_val
-  integer :: coord
-  real    :: r
-  call random_number(r)
-  coord = int(r*max_val + 1.0_dp)
-end function get_rand_coord
+    call random_seed(put=seed)
+  end subroutine init_random_seed
+
 end module random
