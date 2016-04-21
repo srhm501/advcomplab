581d6b66574674a94b97c41afc16c1b8da15a7d8
SRH Morris <srhm501@tlabs180.its.york.ac.uk>
2016-04-21 12:21:35 +0100

    added random number generator

---
+++ src/rand.f90
@@ -0,0 +1,29 @@
+module random
+use quantities, only : dp
+implicit none
+contains
+
+subroutine init_random_seed()
+  implicit none
+  integer, allocatable :: seed(:)
+  integer :: n, un
+  call random_seed(size=n)
+  allocate(seed(n))
+
+  open(newunit=un, file="/dev/urandom", access="stream", &
+       & form="unformatted", action="read", status="old")
+  read(un) seed
+  close(un)
+  
+  call random_seed(put=seed)
+end subroutine init_random_seed
+
+function get_rand_coord(max_val) result(coord)
+  implicit none
+  integer :: max_val
+  integer :: coord
+  real    :: r
+  call random_number(r)
+  coord = int(r*max_val + 1.0_dp)
+end function get_rand_coord
+end module random
