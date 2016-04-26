0f205fda8fddbf032f239b24d150d3929bb48752
SRH Morris <srhm501@tlabs158.its.york.ac.uk>
2016-04-26 13:49:34 +0100

    genatoms now generates coords as well, for now Os are placed randomly as well

--- src/genatoms.f90
+++ src/genatoms.f90
@@ -2,30 +2,55 @@ program genatoms
   use random
   implicit none
 
-  integer, parameter :: num_atoms = 8
+  integer,  parameter :: num_atoms = 8
+  real(dp), parameter :: maxpos = 0.5_dp
+  integer,  parameter :: side = nint(num_atoms**(1./3.))
+  real(dp) :: step = maxpos / (side-1)
 
   real(dp) :: r
-  integer  :: i
+  integer  :: i, j, k, l
+  real(dp) :: xc,yc,zc
 
-  real(dp) :: positions_frac(3,num_atoms/2) = reshape((/ &
-       0.0_dp, 0.0_dp, 0.0_dp, &
-       0.0_dp, 0.5_dp, 0.5_dp, &
-       0.5_dp, 0.0_dp, 0.5_dp, &
-       0.5_dp, 0.5_dp, 0.0_dp  &
-       /), shape(positions_frac))
+  real(dp) :: positions_frac(3,num_atoms)
 
   10 format (4x,a2,3(4x,f12.10))
 
+  l = 0
+  xc = 0.0_dp
+  yc = 0.0_dp
+  zc = 0.0_dp
+
+  ! populate positions_frac
+  do k=1,side
+     do j=1,side
+        do i=1,side
+           positions_frac(:,l) = (/ xc, yc, zc /)
+           xc = xc + step
+           l = l + 1
+        end do
+        xc = 0.0_dp
+        yc = yc + step
+     end do
+     yc = 0.0_dp
+     zc = zc + step
+  end do
+
   call init_random_seed()
 
-  ! half the atoms are O
-  do i=1,num_atoms/2
+  do i=1,num_atoms
+     !if (mod(i,2)==0) then
+     !   write(*,10) ' O', positions_frac(:,i)
+     !   cycle
+     !end if
+
      call random_number(r)
 
-     if (r < 0.5_dp) then
+     if (r < 1.0_dp/3.0_dp) then
         write(*,10) 'Mg', positions_frac(:,i)
-     else
+     else if (r < 2.0_dp/3.0_dp) then
         write(*,10) 'Ca', positions_frac(:,i)
+     else
+        write(*,10) ' O', positions_frac(:,i)
      end if
   end do
 end program
