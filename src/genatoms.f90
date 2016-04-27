8722ae21f42abbc6f36884e443a50bcf7828a544
srhm501 <srhm501@york.ac.uk>
2016-04-27 15:54:25 +0100

    Rewrite of genatoms.f90. New axis type helps check for division by zero so now we can have as few as 4 atoms. Also remove random.mod and genatoms executable from git.

--- src/genatoms.f90
+++ src/genatoms.f90
@@ -2,20 +2,19 @@ program genatoms
   use random
   implicit none
 
-  ! number of atoms on each side of cube
-  integer, parameter :: sidex = 4
-  integer, parameter :: sidey = 4
-  integer, parameter :: sidez = 2
+  type axis
+     ! number of atoms along axis
+     integer  :: numatm
+     
+     ! maximum position of atom along axis
+     real(dp) :: maxpos
+     
+     ! distance between atoms along axis
+     ! doesn't matter what the default is, as long as there is one
+     real(dp) :: step=0.0_dp
+  end type axis
 
-  ! max 'coord' along each axis
-  real(dp), parameter :: maxposx = 1.0_dp
-  real(dp), parameter :: maxposy = 1.0_dp
-  real(dp), parameter :: maxposz = 0.5_dp
-
-  ! distance between each atom along the axis directions
-  real(dp), parameter :: stepx = maxposx / (sidex-1)
-  real(dp), parameter :: stepy = maxposy / (sidey-1)
-  real(dp), parameter :: stepz = maxposz / (sidez-1)
+  type (axis) :: axes(3)
 
   integer  :: i, j, k
   real(dp) :: xc,yc,zc
@@ -25,8 +24,20 @@ program genatoms
 
   10 format (4x,a2,3(4x,f12.10))
 
+  axes(1) = axis(2, 1.0_dp)
+  axes(2) = axis(2, 1.0_dp)
+  axes(3) = axis(1, 0.5_dp)
+
+  do i=1,3
+     if (axes(i)%numatm /= 1) then
+        axes(i)%step = axes(i)%maxpos / (axes(i)%numatm-1)
+     end if
+  end do
+  
   read *, Mgmax, Camax
-  if (Mgmax + Camax /= sidex*sidey*sidez) stop 'Mgmax + Camax must equal side'
+  if (Mgmax + Camax /= (axes(1)%numatm * &
+                        axes(2)%numatm * &
+                        axes(3)%numatm)) stop 'Mgmax + Camax must equal total number of atoms'
 
   call init_random_seed()
 
@@ -34,17 +45,17 @@ program genatoms
   yc = 0.0_dp
   zc = 0.0_dp
 
-  do k=1,sidez
-     do j=1,sidey
-        do i=1,sidex
+  do k=1,axes(3)%numatm
+     do j=1,axes(2)%numatm
+        do i=1,axes(1)%numatm
            write(*,10) rand_atom(), xc, yc, zc
-           xc = xc + stepx
+           xc = xc + axes(1)%step
         end do
         xc = 0.0_dp
-        yc = yc + stepy
+        yc = yc + axes(2)%step
      end do
      yc = 0.0_dp
-     zc = zc + stepz
+     zc = zc + axes(3)%step
   end do
 
 contains
