1837ca7179d65e263da5b0774a46d49c22f940fc
SRH Morris <srhm501@tlabs158.its.york.ac.uk>
2016-04-27 11:06:59 +0100

    remove Oxygens from genatoms

--- src/genatoms.f90
+++ src/genatoms.f90
@@ -2,40 +2,17 @@ program genatoms
   use random
   implicit none
 
-  integer,  parameter :: side = 4
-  integer,  parameter :: num_atoms = side**3
+  integer,  parameter :: side = 2
   real(dp), parameter :: maxpos = 0.5_dp
 
   real(dp) :: step = maxpos / (side-1)
 
   integer  :: i, j, k, l
   real(dp) :: xc,yc,zc
-  character(2) :: atoms(num_atoms)
 
   10 format (4x,a2,3(4x,f12.10))
 
   call init_random_seed()
-
-  l=0
-  do i=1,num_atoms/side
-     k = l*side
-     if (mod(l,2)==0) then
-        do j=1,side,2
-           atoms(j+k) = rand_atom()
-        end do
-        do j=2,side,2
-           atoms(j+k) = ' O'
-        end do
-     else
-        do j=1,side,2
-           atoms(j+k) = ' O'
-        end do
-        do j=2,side,2
-           atoms(j+k) = rand_atom()
-        end do
-     end if
-     l = l + 1
-  end do
 
   l  = 1
   xc = 0.0_dp
@@ -45,7 +22,7 @@ program genatoms
   do k=1,side
      do j=1,side
         do i=1,side
-           write(*,10) atoms(l), xc, yc, zc
+           write(*,10) rand_atom(), xc, yc, zc
            l = l + 1
            xc = xc + step
         end do
