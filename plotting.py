2242f7fa3252f02f67453a07dfaae03bf1852cdd
Oliver Thomas <ot561@york.ac.uk>
2016-04-28 20:25:16 +0100

    python fitting is noit giving correct shape for efromation.png

--- plotting.py
+++ plotting.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def fixy(x,y,coeff):
+    #subtract mx+c
+    yfixed=y-(coeff[1]*x+coeff[2])
+    return yfixed
+
 datafile=raw_input('enter data file name: ')
 x, y = np.genfromtxt(datafile, unpack=True)
 
@@ -10,6 +15,7 @@ n=raw_input('enter degrees of freedom for fit')
 #Calculate best fit order n 
 p = np.polyfit(x,y,n)
 #print coefficients
+#p[0]=ax^n, p[1]=bx^(n-1) etc
 print p
 
 #rewrite fit as a function
@@ -21,8 +27,16 @@ plt.plot(x, y, 'ro', xp, fit(xp), 'r-')
 
 plt.ylabel('Energy, eV')
 plt.xlabel('Percentage composition of Mg/Ca')
-
+plt.title('Energy against Composition of Mg/Ca')
 #save graph 
 d_name = datafile + '.png'
 plt.savefig(d_name, format='png')
-plt.close()
+plt.clf()
+
+#plot fixed energy of formation
+plt.plot(xp, fixy(xp,fit(xp), p), 'r-')
+plt.ylabel('Energy, eV')
+plt.xlabel('Percentage composition of Mg/Ca')
+plt.title('Formation Energy for Composition of Mg/Ca')
+#save graph
+plt.savefig('eform.png',format='png')
