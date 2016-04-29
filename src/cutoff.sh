123dcb83bcd1399b5212ebaa7a89dfe5e1a1e138
Oliver Thomas <ot561@york.ac.uk>
2016-04-29 16:58:42 +0100

    cutoff now works until change in energies is minimised

--- src/cutoff.sh
+++ src/cutoff.sh
@@ -6,31 +6,55 @@ root=..
 cd $current
 cell=$root/cell
 data=$root/dat
-rm $data/cutoff.dat
 
-for system in $cell/*.cell
+
+
+for system in $cell/0.cell
 do
 
-if ! [ -s $data/cutoff.dat ] ; then
-for i in {250..400..10}
+#remove.cell
+cutoff=${system::-5}
+cutoff=${cutoff:8}
+rm $data/$cutoff.dat
+#if ! [ -s $data/$cutoff.dat ] ; then
+
+########### CHANGE ME ##########
+incr=20
+i=200
+
+######## DON'T TOUTCH #########
+energy=100000
+oldenergy=0
+oldi=1
+exitcond=1
+
+while [ $exitcond -ne 0 ]
 do
-file2=$cell/cutoff.castep
+exitcond=$(echo $energy $oldenergy | awk '{if (sqrt(($1-$2)^2) > 0.001) print 1; else print 0;}')
+if [ $exitcond -eq 0 ]; then
+break
+fi 
+
+file2=$cell/$cutoff.castep
 rm $file2
-file=$cell/cutoff.param
+file=$cell/$cutoff.param
+
 sed -i "/cut_off_energy/c\cut_off_energy     : "$i" eV." $file
 echo 'calculating energy using a cutoff energy of' $i
-mpirun -np 1 $cell/castep.mpi $cell/cutoff
+time mpirun -np 1 $cell/castep.mpi $cell/$cutoff
 
+oldenergy=$energy
 energy=$(exec ./findenergy.sh $file2)
-echo $i $energy >> $data/cutoff.dat
+echo $i $energy >> $data/$cutoff.dat
 
-oldenergy=$enery
+oldi=$i
+i=$((i+incr))
 done
 cd $old
 
-else 
+#else 
 echo "data file aready there!"
-fi
+#fi
 
-./plot.sh $data/cutoff.dat 4
+./plot.sh $data/$cutoff.dat 4
 done
