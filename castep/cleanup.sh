98231cb7b389bd124083cd2afc1e99469bf8e543
O Thomas <ot561@jorvik.york.ac.uk>
2016-04-30 00:35:37 +0100

    Ran on jorvik for mg0, mg25%, 50%, 75%, 100%

---
+++ castep/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#old="$(pwd)"
+#current="$(dirname "$0")"
+#cd $current
+
+rm ./cell/*.bib ./cell/*_bin ./cell/*.bands ./cell/*.err ./cell/*.check ./cell/*.cst_esp 2>/dev/null
+
+rm ./*.bib ./*_bin ./*.bands ./*.err ./*.check ./*.cst_esp 2>/dev/null
+
+
+#cd $old
