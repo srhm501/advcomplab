fcee1ce8eb8fc3078fd5df15bdc4d297fea380fe
srhm501 <srhm501@york.ac.uk>
2016-04-30 23:54:49 +0100

    new genatoms for 3x3 cell

--- src/gencell.sh
+++ src/gencell.sh
@@ -4,21 +4,22 @@ old="$(pwd)"
 current="$(dirname "$0")"
 root=..
 cd $current
-
+echo $old
 if [ ! -f $root/genatoms ] ; then
-    exec ../make
+    exec $root/make
 fi
 
-for i in {1..17}
+for i in {0..5}
 do
-echo $i
-head -n7 $root/cell.master > $i.cell
-$root/genatoms <<EOF >> $i.cell
-$i
-EOF
+    file=27_$i.cell
+    echo $i
+    head -n7 $root/cell.master > $file
+    $root/genatoms $i >> $file
 
-tail -n19 $root/cell.master | head -n10 >> $i.cell
+    tail -n19 $root/cell.master | head -n10 >> $file
 done
+
 mv *.cell ../
+
 cd $old
 
