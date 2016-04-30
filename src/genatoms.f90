d92b41912ab015e445386f75869927c284a387bf
srhm501 <srhm501@york.ac.uk>
2016-04-30 23:25:47 +0100

    genatoms rewritten to create a 3x3 cell bottom layer is pure MgO top layer is pure CaO middle layer changes depending on command line argument

--- src/genatoms.f90
+++ src/genatoms.f90
@@ -1,88 +1,96 @@
 program genatoms
-  use interface_t
   implicit none
   
   integer, parameter :: dp = selected_real_kind(15,300)
 
-  type axis
-     ! number of atoms along axis
-     integer  :: numatm
-     
-     ! maximum position of atom along axis (1.0 == no vacuum gap)
-     real(dp) :: maxpos=1.0
-     
-     ! distance between atoms along axis
-     ! doesn't matter what the default is, as long as there is one
-     real(dp) :: step=0.0_dp
-  end type axis
+  ! atoms per side of cube cell
+  ! keep as 3 for now
+  integer,  parameter :: N = 3
 
-  type(axis) :: axes(3)
-  
-  integer  :: intercase
-  character(2) :: intertype(2,4,2)
+  ! distance between atoms
+  real(dp), parameter :: step = 1.0_dp/N
 
-  integer  :: purelines = 2
+  type atom
+     character(2) :: atom_type = "XX"
+     real(dp)     :: coord(3)  = [0.0_dp, 0.0_dp, 0.0_dp]
+  end type atom
 
-  integer  :: i, j, k
+  type (atom) :: lattice(N**3)
+
+  integer  :: i, j, k, l
   real(dp) :: xc,yc,zc
 
-  10 format (4x,a2,3(4x,f12.10))
 
-  read *, intercase
-  intertype = get_intertype(intercase)
+  character(1) :: iface
+  character(2) :: middle(4) = "Mg"
 
-  axes(1) = axis(2)
-  axes(2) = axis(4)
-  axes(3) = axis(6)
+  call get_command_argument(1,iface)
 
-
-
-  where (axes(:)%numatm /= 1) axes(:)%step = axes(:)%maxpos / (axes(:)%numatm-1)
-
+  ! create coordinates
   xc = 0.0_dp
   yc = 0.0_dp
   zc = 0.0_dp
-
-  ! upper layer (all Mg)
-  do k=1,purelines
-     do j=1,axes(2)%numatm
-        do i=1,axes(1)%numatm
-           write(*,10) 'Mg', xc, yc, zc
-           xc = xc + axes(1)%step
+  l = 1
+  do k=1,N
+     zc = zc + (k-1)*step
+     do j=1,N
+        yc = yc + (j-1)*step
+        do i=1,N
+           xc = xc + (i-1)*step
+           lattice(l)%coord = [ xc, yc, zc ]
+           l = l + 1
         end do
         xc = 0.0_dp
-        yc = yc + axes(2)%step
      end do
      yc = 0.0_dp
-     zc = zc + axes(3)%step
   end do
 
-  ! middle layer (interface)
-  do k=1,axes(3)%numatm-2*purelines
-     do j=1,axes(2)%numatm
-        do i=1,axes(1)%numatm
-           write(*,10) intertype(i,j,k), xc, yc, zc
-           xc = xc + axes(1)%step
-        end do
-        xc = 0.0_dp
-        yc = yc + axes(2)%step
-     end do
-     yc = 0.0_dp
-     zc = zc + axes(3)%step
+  ! create pure top and bottom layers and init middle
+  lattice(01:09)%atom_type = gen_layer(0,"Mg")
+  lattice(10:18)%atom_type = gen_layer(1,"Mg")
+  lattice(19:27)%atom_type = gen_layer(0,"Ca")
+
+  select case (iface)
+  case ('0')
+  case ('1')
+     middle(1) = "Ca"
+  case ('2')
+     middle(1:2) = "Ca"
+  case ('3')
+     middle(1:3) = "Ca"
+  case ('4')
+     middle(:) = "Ca"
+  case ('5')
+     middle(1) = "Ca"
+     middle(3) = "Ca"
+  case default
+     stop "iface not valid"
+  end select
+
+  j = 1
+  do i=10,18
+     if (lattice(i)%atom_type /= " O") then
+        lattice(i)%atom_type = middle(j)
+        j = j + 1
+     end if
+  end do
+  
+  do i=1,N**3
+     write(*,'(a2,3(1x,f4.2))') lattice(i)
   end do
 
-  ! bottom layer (all Ca)
-  do k=1,purelines
-     do j=1,axes(2)%numatm
-        do i=1,axes(1)%numatm
-           write(*,10) 'Ca', xc, yc, zc
-           xc = xc + axes(1)%step
-        end do
-        xc = 0.0_dp
-        yc = yc + axes(2)%step
-     end do
-     yc = 0.0_dp
-     zc = zc + axes(3)%step
-  end do
-
+contains
+  function gen_layer(offset,atm) result(layer)
+    integer      :: offset
+    character(2) :: atm
+    character(2) :: layer(9)
+    integer      :: i
+    do i=1,9
+       if (mod(i+offset,2)/=0) then
+          layer(i) = atm
+       else
+          layer(i) = " O"
+       end if
+    end do
+  end function gen_layer
 end program
