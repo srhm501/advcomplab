1fca1a2abf350aba8304e786539cc65cfb253694
Oliver Thomas <ot561@york.ac.uk>
2016-04-30 23:14:55 +0100

    changing write O every even i does not work just replaces all mg with O

--- src/genatoms.f90
+++ src/genatoms.f90
@@ -43,7 +43,11 @@ program genatoms
   do k=1,purelines
      do j=1,axes(2)%numatm
         do i=1,axes(1)%numatm
-           write(*,10) 'Mg', xc, yc, zc
+	   if(mod(i,2)==0) then
+             write(*,10) 'Mg', xc, yc, zc
+	   else
+	     write(*,10) 'O', xc, yc, zc
+ 	   end if
            xc = xc + axes(1)%step
         end do
         xc = 0.0_dp
