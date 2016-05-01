5b4100f3825d457b0bf75994b46897ba25bb0a89
Oliver Thomas <ot561@york.ac.uk>
2016-05-01 21:00:08 +0100

    whoops, cleanup was deleting useful stuff

--- castep/cleanup.sh
+++ castep/cleanup.sh
@@ -4,9 +4,7 @@
 #current="$(dirname "$0")"
 #cd $current
 
-rm ./cell/*.bib ./cell/*_bin ./cell/*.bands ./cell/*.err ./cell/*.check ./cell/*.cst_esp 2>/dev/null
-
-rm ./*.bib ./*_bin ./*.bands ./*.err ./*.check ./*.cst_esp 2>/dev/null
+rm ./*.bib ./*_bin ./*.bands ./*.check ./*.cst_esp 2>/dev/null
 rm ./CQM*
 
 
