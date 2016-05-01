900d0361fee96e4bdefec71d32e06157cfe4c8eb
Oliver Thomas <ot561@york.ac.uk>
2016-05-01 20:54:16 +0100

    fixed cleanup.sh to remove all CQM log files

--- castep/cleanup.sh
+++ castep/cleanup.sh
@@ -7,6 +7,7 @@
 rm ./cell/*.bib ./cell/*_bin ./cell/*.bands ./cell/*.err ./cell/*.check ./cell/*.cst_esp 2>/dev/null
 
 rm ./*.bib ./*_bin ./*.bands ./*.err ./*.check ./*.cst_esp 2>/dev/null
+rm ./CQM*
 
 
 #cd $old
