354000b076e69d3a1c6f6c6f590150efa06e61dc
srhm501 <srhm501@york.ac.uk>
2016-05-04 19:46:00 +0100

    generated new cell files in castep new are 6-10 I think

--- src/genatoms.f90
+++ src/genatoms.f90
@@ -1,96 +1,97 @@
 program genatoms
+  use interface_t
   implicit none
   
   integer, parameter :: dp = selected_real_kind(15,300)
 
-  ! atoms per side of cube cell
-  ! keep as 3 for now
-  integer,  parameter :: N = 3
+  type axis
+     ! number of atoms along axis
+     integer  :: numatm=3
+     
+     ! maximum position of atom along axis
+     real(dp) :: maxpos=2.0_dp/3.0_dp
+     
+     ! distance between atoms along axis
+     ! doesn't matter what the default is, as long as there is one
+     real(dp) :: step=0.0_dp
+  end type axis
 
-  ! distance between atoms
-  real(dp), parameter :: step = 0.67_dp/(N-1)
+  type(axis) :: axes(3)
 
-  type atom
-     character(2) :: atom_type = "XX"
-     real(dp)     :: coord(3)  = [0.0_dp, 0.0_dp, 0.0_dp]
-  end type atom
+  ! which interface to use
+  integer  :: intercase
 
-  type (atom) :: lattice(N**3)
+  ! the interface layer
+  character(2) :: intertype(3,3)
 
-  integer  :: i, j, k, l
+  ! how many lines of pure MgO and CaO
+  integer  :: purelines = 1
+
+  integer  :: i, j, k
   real(dp) :: xc,yc,zc
 
-  character(1) :: iface
-  character(2) :: middle(4) = "Mg"
+  10 format (4x,a2,3(4x,f12.10))
 
-  call get_command_argument(1,iface)
+  read *, intercase
+  intertype = get_intertype(intercase)
 
-  ! create coordinates
+  axes(:) = axis()
+
+  where (axes(:)%numatm /= 1) axes(:)%step = axes(:)%maxpos / (axes(:)%numatm-1)
+
   xc = 0.0_dp
   yc = 0.0_dp
   zc = 0.0_dp
 
-  l = 1
-  do k=1,N
-     do j=1,N
-        do i=1,N
-           lattice(l)%coord = [ xc, yc, zc ]
-           xc = xc + step
-           l = l + 1
+  ! upper layer (all Mg)
+  do k=1,purelines
+     do j=1,axes(2)%numatm
+        do i=1,axes(1)%numatm
+           if(mod(k+j+i,2)==0) then
+              write(*,10) 'Mg', xc, yc, zc
+           else
+              write(*,10) 'O', xc, yc, zc
+           end if
+           xc = xc + axes(1)%step
         end do
-        yc = yc + step
         xc = 0.0_dp
+        yc = yc + axes(2)%step
      end do
-     zc = zc + step
      yc = 0.0_dp
+     zc = zc + axes(3)%step
   end do
 
-  ! create pure top and bottom layers and init middle
-  lattice(01:09)%atom_type = gen_layer(0,"Mg")
-  lattice(10:18)%atom_type = gen_layer(1,"Mg")
-  lattice(19:27)%atom_type = gen_layer(0,"Ca")
-
-  select case (iface)
-  case ('0')
-  case ('1')
-     middle(1) = "Ca"
-  case ('2')
-     middle(1:2) = "Ca"
-  case ('3')
-     middle(1:3) = "Ca"
-  case ('4')
-     middle(:) = "Ca"
-  case ('5')
-     middle(1) = "Ca"
-     middle(3) = "Ca"
-  case default
-     stop "iface not valid"
-  end select
-
-  j = 1
-  do i=10,18
-     if (lattice(i)%atom_type /= " O") then
-        lattice(i)%atom_type = middle(j)
-        j = j + 1
-     end if
-  end do
-  
-  do i=1,N**3
-     write(*,'(4x,a2,3(4x,f12.10))') lattice(i)
+  ! middle layer (interface)
+  do k=1,axes(3)%numatm-2*purelines
+     do j=1,axes(2)%numatm
+        do i=1,axes(1)%numatm
+               write(*,10) intertype(i,j), xc, yc, zc
+           xc = xc + axes(1)%step
+        end do
+        xc = 0.0_dp
+        yc = yc + axes(2)%step
+     end do
+     yc = 0.0_dp
+     zc = zc + axes(3)%step
   end do
 
-contains
-  function gen_layer(offset,atm) result(layer)
-    integer      :: offset
-    character(2) :: atm
-    character(2) :: layer(9)
-    integer      :: i
-    do i=1,9
-       if (mod(i+offset,2)/=0) then
-          layer(i) = atm
-       else
-          layer(i) = " O"
-       end if
-    end do
-  end function gen_layer
+  ! bottom layer (all Ca)
+  do k=1,purelines
+     do j=1,axes(2)%numatm
+        do i=1,axes(1)%numatm
+           if(mod(k+j+i,2)==0) then
+               write(*,10) 'Ca', xc, yc, zc
+            else
+               write(*,10) 'O', xc, yc, zc
+            end if
+           xc = xc + axes(1)%step
+        end do
+        xc = 0.0_dp
+        yc = yc + axes(2)%step
+     end do
+     yc = 0.0_dp
+     zc = zc + axes(3)%step
+  end do
+
 end program
+
--- src/gencell.sh
+++ src/gencell.sh
@@ -9,12 +9,14 @@ if [ ! -f $root/genatoms ] ; then
     exec $root/make
 fi
 
-for i in {0..5}
+for i in {0..10}
 do
     file=27_$i.cell
     echo $i
     head -n7 $root/cell.master > $file
-    $root/genatoms $i >> $file
+    $root/genatoms <<EOF >> $file
+$i
+EOF
 
     tail -n19 $root/cell.master | head -n10 >> $file
 done
