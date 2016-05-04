ce643fbce39f5bc7a0b10b5d05373575daa327d2
SRH Morris <srhm501@tlabs173.its.york.ac.uk>
2016-05-04 11:44:40 +0100

    update gencell

--- src/gencell.sh
+++ src/gencell.sh
@@ -9,18 +9,16 @@ if [ ! -f $root/genatoms ] ; then
     exec ../make
 fi
 
-#for i in {0..8..1}
-#do
-i=$1
-head -n7 $root/cell.master > $i.cell
-../genatoms <<EOF >> $i.cell
-0
-24
+for i in {0..5}
+do
+file=27_$i.cell
+head -n7 $root/cell.master > $file
+../genatoms <<EOF >> $file
+$i
 EOF
 
-#$root/genatoms >> $1
-tail -n19 $root/cell.master | head -n10 >> $i.cell
-#done
+tail -n19 $root/cell.master | head -n10 >> $file
+done
 mv *.cell ../
 cd $old
 
