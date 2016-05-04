72902613edffc5b5c696d94a1163773ebcd0021e
SRH Morris <srhm501@tlabs173.its.york.ac.uk>
2016-05-04 11:49:52 +0100

    fix indentation

--- src/gencell.sh
+++ src/gencell.sh
@@ -11,13 +11,13 @@ fi
 
 for i in {0..5}
 do
-file=27_$i.cell
-head -n7 $root/cell.master > $file
-../genatoms <<EOF >> $file
+    file=27_$i.cell
+    head -n7 $root/cell.master > $file
+    ../genatoms <<EOF >> $file
 $i
 EOF
 
-tail -n19 $root/cell.master | head -n10 >> $file
+    tail -n19 $root/cell.master | head -n10 >> $file
 done
 mv *.cell ../
 cd $old
