557bae2edff8c773dddb7d33a423eb38dc11fc52
SRH Morris <srhm501@tlabs165.its.york.ac.uk>
2016-05-05 13:11:10 +0100

    remove unneeded stuff from findenergy.sh

--- src/findenergy.sh
+++ src/findenergy.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
+# finds the final energy a from .castep file
+
 file=$1
-celf=$2
-#Find how many Mg and Ca lines in cell file
-# numg=$(grep -c "Mg" $celf)
-# numca=$(grep -c "Ca" $celf)
-#     
-# #mg/total atoms
-# ratio=$(echo $numg $numca | awk '{print $1/($1+$2)}')
 
 search=$(grep "Total energy corrected for finite basis set" $file)
 energy=$(echo $search | awk '{print $9}')
 
 echo $energy
-# 
-# python plotting.py << EOF
-# energy.dat
-# 2
-# 1
-# EOF
